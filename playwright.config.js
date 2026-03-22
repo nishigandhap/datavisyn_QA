5908fd86e6f1e527a3551af4f916e442cc0cdce8
Nishigandha <patilnisha3939@gmail.com>
2026-03-22 12:37:45 +0530

    update report +version

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   reporter: [
     ['list'],                  // console output
     ['allure-playwright'],     // Allure reporting
-    ['html']                   // optional HTML report
+    ['html', { outputFolder: 'playwright-report', open: 'never' }] // optional HTML report
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
